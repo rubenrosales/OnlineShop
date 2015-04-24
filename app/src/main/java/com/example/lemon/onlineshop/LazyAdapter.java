3de7d2d7a2843c4f29ba820c1dce00e6305dd92b
rubenrosales <ruben.ros@csu.fullerton.edu>
2015-04-23 20:35:59 -0700

    Added missing files

---
+++ app/src/main/java/com/example/lemon/onlineshop/LazyAdapter.java
@@ -0,0 +1,61 @@
+package com.example.lemon.onlineshop;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LazyAdapter extends BaseAdapter {
+
+    private Activity activity;
+
+    private static LayoutInflater inflater=null;
+    public ImageLoader imageLoader;
+
+    private String[] artistArray;
+    private String[] songArray;
+    private String[] artworkArray;
+
+    public LazyAdapter(Activity a, String[] artwork,String[] artist, String[] song ) {
+
+        activity = a;
+        artworkArray = artwork;
+        artistArray = artist;
+        songArray = song;
+
+        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        imageLoader=new ImageLoader(activity.getApplicationContext());
+    }
+
+    public int getCount() {
+        return artworkArray.length;
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View vi=convertView;
+
+            if (convertView == null)
+                vi = inflater.inflate(R.layout.item, null);
+
+            TextView text = (TextView) vi.findViewById(R.id.text);
+            ImageView image = (ImageView) vi.findViewById(R.id.image);
+            text.setText(artistArray[position]+ " - " + songArray[position]);
+            imageLoader.DisplayImage(artworkArray[position], image);
+
+
+        return vi;
+    }
+}
